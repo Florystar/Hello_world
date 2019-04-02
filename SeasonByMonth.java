f332fb229cdebba8ff85d8f231c2c0e0d02ce365
szhyvol <szhyvol@softserveinc.com>
2019-04-02 15:16:29 +0300

    Correcting one mistake

--- SeasonByMonth.java
+++ SeasonByMonth.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 public class SeasonByMonth {
 
-    public static void main(String arg[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         Season season;
         season = null;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
