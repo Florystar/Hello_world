157c2a7f5f53c3ebec39a99f7470831d5a843de0
szhyvol <szhyvol@softserveinc.com>
2019-04-02 09:26:42 +0300

    Updating to Code Convention

--- SeasonByMonth.java
+++ SeasonByMonth.java
@@ -4,45 +4,44 @@ import java.io.InputStreamReader;
 
 public class SeasonByMonth {
 
-    public enum Season{
-        WINTER, SPRING, SUMMER, AUTUMN
-    }
-
     public static void main(String arg[]) throws IOException {
         Season season;
         season = null;
-        BufferedReader br = new BufferedReader(
-                new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please, enter the month");
         String input = br.readLine();
 
-        String month=input.toLowerCase();
-        switch (month){
-            case "december":
-            case "january":
-            case "february":
-                season = Season.WINTER;
-                break;
-            case "march":
-            case "april":
-            case "may":
-                season = Season.SPRING;
-                break;
-            case "june":
-            case "july":
-            case "august":
-                season = Season.SUMMER;
-                break;
-            case "september":
-            case "october":
-            case "november":
-                season = Season.AUTUMN;
-                break;
-            default:
-                System.out.println("No such month");
+        String month = input.toLowerCase();
+        switch (month) {
+        case "december":
+        case "january":
+        case "february":
+            season = Season.WINTER;
+            break;
+        case "march":
+        case "april":
+        case "may":
+            season = Season.SPRING;
+            break;
+        case "june":
+        case "july":
+        case "august":
+            season = Season.SUMMER;
+            break;
+        case "september":
+        case "october":
+        case "november":
+            season = Season.AUTUMN;
+            break;
+        default:
+            System.out.println("No such month");
         }
         if (season != null)
             System.out.println("The month " + input.toUpperCase() + " relates to the following season " + season);
+    }
+
+    public enum Season {
+        WINTER, SPRING, SUMMER, AUTUMN
     }
 }
 
