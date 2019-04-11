baeaa0335f5872232a36294d78f63685d81df54d
szhyvol <szhyvol@softserveinc.com>
2019-04-12 00:01:03 +0300

    Changing method to remove for cycle. But 2 tests are still failed

--- Student.java
+++ Student.java
@@ -1,15 +1,25 @@
 public class Student {
     private String name;
     private int rating;
+    static int count=0;
+    static double sum=0;
 
     //Create default constructor
     public Student() {
+        count++;
     }
 
     //Create constructor with parameters
     public Student(String name, int rating) {
         this.name = name;
         this.rating = rating;
+        count++;
+    }
+
+    //Create constructor with parameters
+    public Student(String name) {
+        this.name = name;
+        count++;
     }
 
     //Creation methods - Getters and Setters - to access fields
@@ -27,6 +37,7 @@ public class Student {
 
     public void setRating(int rating) {
         this.rating = rating;
+        sum+=this.rating;
     }
 
     //Method for selecting a better Student
@@ -45,42 +56,43 @@ public class Student {
     }
 
     //Method calculating of average rating
-    public static double average(int... rating) {
-        double sum = 0;
-        int count = 0;
-        for (int i = 0; i < rating.length; i++) {
-            sum += rating[i];
-            count++;
+    public static double average() {
+        double average = 0;
+        if (count==0)  {
+            return average;
+        } else {
+            return sum / count;
         }
-        double average = sum / count;
-        return average;
     }
 
     //Method of changing rating
-    public static Student[] changeStudentsRating(int changeRating, Student... s) {
-        for (int i = 0; i < s.length; i++) {
-            s[i].rating = s[i].rating + changeRating;
-        }
-        return s;
+    public double changeStudentsRating(int changeRating) {
+            sum-= this.rating;
+            this.setRating(changeRating);
+        return sum;
     }
 
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.setName("Octavian");
+        Student s1 = new Student("Octavian");
         s1.setRating((int) (Math.random() * 100));
-        Student s2 = new Student("Varvara", (int) (Math.random() * 100));
-        Student s3 = new Student("Denys", (int) (Math.random() * 100));
+        Student s2 = new Student("Varvara");
+        s2.setRating((int) (Math.random() * 100));
+        Student s3 = new Student("Denys");
+        s3.setRating((int) (Math.random() * 100));
 
         System.out.println("The rating of first student is " + s1.rating);
         System.out.println("The rating of second student is " + s2.rating);
         System.out.println("The rating of third student is " + s3.rating);
         System.out.println("-------------------------------------------------------------");
         System.out.println(String.format(
-                "The average rating of all students before changing: %.2f", average(s1.rating, s2.rating, s3.rating)));
-        changeStudentsRating(15, s2);
+                "The average rating of all students before changing: %.2f", average()));
+        s1.changeStudentsRating(25);
+        System.out.println("The rating of first student AFTER change is " + s1.rating);
+        System.out.println("The rating of second student AFTER change is " + s2.rating);
+        System.out.println("The rating of third student AFTER change is " + s3.rating);
+        System.out.println("-------------------------------------------------------------");
         System.out.println(String.format(
-                "The new average rating of all students after changing: %.2f", average( s1.rating, s2.rating,
-                        s3.rating)));
+                "The new average rating of all students after changing: %.2f", average()));
     }
 }
 
